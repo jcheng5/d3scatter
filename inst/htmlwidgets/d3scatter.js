3d83e7eed95bd55ddbd5bab83fd803f95399605b
Joe Cheng <joe@rstudio.com>
2016-11-21 14:15:24 -0800

    Hook up to crosstalk

--- inst/htmlwidgets/d3scatter.js
+++ inst/htmlwidgets/d3scatter.js
@@ -8,8 +8,43 @@ HTMLWidgets.widget({
 
     var firstRun = true;
     var scatter = d3scatter(el).width(width).height(height);
+
+    var ct_selection = null;
+    var ct_filter = null;
+
+    scatter.on("brush", function(keys) {
+      if (ct_selection) {
+        ct_selection.set(keys);
+      }
+    });
+
     return {
       renderValue: function(value) {
+
+        if (ct_selection) {
+          ct_selection.close();
+          ct_selection = null;
+        }
+        if (ct_filter) {
+          ct_filter.close();
+          ct_filter = null;
+        }
+
+        if (value.group) {
+          ct_selection = new crosstalk.SelectionHandle(value.group);
+          ct_selection.on("change", function(e) {
+            if (e.sender !== ct_selection) {
+              scatter.clearBrush();
+            }
+            scatter.selection(e.value);
+          });
+
+          ct_filter = new crosstalk.FilterHandle(value.group);
+          ct_filter.on("change", function(e) {
+            scatter.filter(e.value);
+          });
+        }
+
         scatter
           .x_var(value.x_var)
           .y_var(value.y_var)
